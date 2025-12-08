a0df0393ff00d832a2ebabb2888332605e971fb3
eric <kuchaguangjie@gmail.com>
2025-12-08 22:13:05 +0800

    chore: ...

--- playground/src/views/system/user/modules/form.vue
+++ playground/src/views/system/user/modules/form.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import type { SystemUserApi } from '#/api/system/user';
+import { createUser, getUserRoles, updateUser } from '#/api/system/user';
 
 import { computed, nextTick, ref } from 'vue';
 
@@ -7,7 +8,6 @@ import { useVbenDrawer } from '@vben/common-ui';
 
 import { useVbenForm } from '#/adapter/form';
 import { getRoleAll } from '#/api/system/role';
-import { createUser, getUserRoles, updateUser } from '#/api/system/user';
 import { $t } from '#/locales';
 
 import { useFormSchema } from '../data';
@@ -63,6 +63,35 @@ const [Drawer, drawerApi] = useVbenDrawer({
       if (roleOptions.value.length === 0) {
         await loadRoleOptions(data.username);
       }
+
+      if (data.id) {
+        // edit
+        formApi.updateSchema([
+          {
+            component: 'Input',
+            fieldName: 'username',
+            label: '', // 空标签使其不显示
+            componentProps: {
+              style: { display: 'none' }, // 隐藏输入框
+              disabled: true, // 使其不可编辑
+            },
+          },
+        ]);
+      } else {
+        // new
+        formApi.updateSchema([
+          {
+            component: 'Input',
+            fieldName: 'username',
+            label: $t('system.user.username'),
+            rules: 'required',
+            componentProps: {
+              style: { display: 'block' },
+              disabled: false,
+            },
+          },
+        ]);
+      }
     }
   },
 });
@@ -78,9 +107,6 @@ async function loadRoleOptions(username: string) {
       value: role.code,
     }));
 
-    // 获取用户当前拥有的角色
-    const userRoles = await getUserRoles(username);
-
     // 动态更新表单字段的选项
     formApi.updateSchema([
       {
@@ -91,8 +117,11 @@ async function loadRoleOptions(username: string) {
       },
     ]);
 
-    // 选中用户已有角色
-    await formApi.setFieldValue('roleCodes', userRoles);
+    // 修改
+    if (username) {
+      const userRoles = await getUserRoles(username); // 获取用户当前拥有的角色
+      await formApi.setFieldValue('roleCodes', userRoles); // 选中用户已有角色
+    }
   } catch (error) {
     console.error('加载角色选项失败:', error);
   } finally {
