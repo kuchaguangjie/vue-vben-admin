4cb9a14596f5f7a3529882d4e579a4a44e9cbcfb
eric <kuchaguangjie@gmail.com>
2025-12-18 22:14:16 +0800

    chore: ...

--- playground/src/views/system/role/modules/form.vue
+++ playground/src/views/system/role/modules/form.vue
@@ -4,14 +4,7 @@ import type { DataNode } from 'ant-design-vue/es/tree';
 import type { Recordable } from '@vben/types';
 
 import type { SystemApiApi } from '#/api';
-import { getApiTree, getInheritRoles } from '#/api';
 import type { SystemRoleApi } from '#/api/system/role';
-import {
-  createRole,
-  getRoleAll,
-  getRoleApis,
-  updateRole,
-} from '#/api/system/role';
 
 import { computed, nextTick, ref } from 'vue';
 
@@ -21,7 +14,14 @@ import { IconifyIcon } from '@vben/icons';
 import { Spin } from 'ant-design-vue';
 
 import { useVbenForm } from '#/adapter/form';
+import { getApiTree, getInheritRoles } from '#/api';
 import { getMenuTree } from '#/api/system/menu';
+import {
+  createRole,
+  getRoleAll,
+  getRoleApis,
+  updateRole,
+} from '#/api/system/role';
 import { $t } from '#/locales';
 
 import {
