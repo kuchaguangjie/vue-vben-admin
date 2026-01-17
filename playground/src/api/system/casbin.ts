8004ab7f36887130c5c3e0f16e50819131d4ba27
eric <kuchaguangjie@gmail.com>
2026-01-17 11:16:21 +0800

    chore: ...

--- playground/src/api/system/casbin.ts
+++ playground/src/api/system/casbin.ts
@@ -7,11 +7,11 @@ export namespace SystemCasbinApi {
 }
 
 /**
- * 获取 casbin status
+ * 获取 casbin 手动操作 状态
  */
-async function getCasbinStatus() {
+async function getCasbinManualStatus() {
   return requestClient.get<Array<SystemCasbinApi.SystemCasbin>>(
-    '/casbin/status',
+    '/casbin/manualStatus',
   );
 }
 
@@ -24,4 +24,11 @@ async function loadCasbin() {
   );
 }
 
-export { getCasbinStatus, loadCasbin };
+/**
+ * 获取 casbin 统计
+ */
+async function getCasbinStat() {
+  return requestClient.get<Array<SystemCasbinApi.SystemCasbin>>('/casbin/stat');
+}
+
+export { getCasbinManualStatus, getCasbinStat, loadCasbin };
--- playground/src/views/system/casbin/list.vue
+++ playground/src/views/system/casbin/list.vue
@@ -6,37 +6,92 @@ import { $t } from '@vben/locales';
 
 import { Divider, message } from 'ant-design-vue';
 
-import { getCasbinStatus, loadCasbin } from '#/api/system/casbin';
+import {
+  getCasbinManualStatus,
+  getCasbinStat,
+  loadCasbin,
+} from '#/api/system/casbin';
+import { formatBackendTime } from '#/utils/value-format';
 
-const displayData = ref<any>(null);
+const manualStatusData = ref<any>(null);
+const statData = ref<any>(null);
 onMounted(() => {
-  handleStatus();
+  handleManualStatus();
 });
 
-async function handleStatus(showSuccess: boolean = false) {
-  displayData.value = await getCasbinStatus();
+async function handleManualStatus(showSuccess: boolean = false) {
+  manualStatusData.value = await getCasbinManualStatus();
   if (showSuccess) message.success($t('common.messages.success'));
 }
 
 async function handleLoad() {
   await loadCasbin();
-  message.success($t('system.casbin.loadSuccess'));
+  message.success($t('system.casbin.manualStatus.loadSuccess'));
   // 失败时 (e.g http 500), 自动从 显示错误提示 (result.message);
-  await handleStatus(); // 刷新状态
+  await handleManualStatus(); // 刷新状态
+}
+
+async function handleStat() {
+  statData.value = await getCasbinStat();
+  message.success($t('common.messages.success'));
 }
 </script>
 <template>
   <Page auto-content-height>
+    <Divider>{{ $t('system.casbin.manualStatus.title') }}</Divider>
     <div class="flex w-full items-center justify-between p-4">
-      <VbenButton type="primary" @click="handleStatus(true)">
-        {{ $t('system.casbin.btnGetStatus') }}
+      <VbenButton type="primary" @click="handleManualStatus(true)">
+        {{ $t('system.casbin.manualStatus.btnGetStatus') }}
       </VbenButton>
       <VbenButton type="primary" @click="handleLoad()">
-        {{ $t('system.casbin.btnLoad') }}
+        {{ $t('system.casbin.manualStatus.btnLoad') }}
       </VbenButton>
     </div>
 
-    <Divider>{{ $t('system.casbin.status') }}</Divider>
+    <div>
+      <div>
+        {{ $t('system.casbin.manualStatus.loadCount') }}:
+        {{ manualStatusData?.loadCount }}
+      </div>
+      <div v-if="manualStatusData?.loadCount > 0">
+        {{ $t('system.casbin.manualStatus.loadAt') }}:
+        {{ formatBackendTime(manualStatusData?.loadAt) }}
+      </div>
+    </div>
+
+    <Divider>{{ $t('system.casbin.stat.title') }}</Divider>
+    <div class="flex w-full items-center justify-between p-4">
+      <VbenButton type="primary" @click="handleStat()">
+        {{ $t('system.casbin.stat.btnStat') }}
+      </VbenButton>
+    </div>
+
+    <div v-if="statData">
+      <div>
+        {{ $t('system.casbin.stat.statAt') }}:
+        {{ formatBackendTime(statData?.statAt) }}
+      </div>
+      <div>
+        {{ $t('system.casbin.stat.ruleCount') }}: {{ statData?.ruleCount }}
+      </div>
+      <div>
+        {{ $t('system.casbin.stat.gRuleCount') }}: {{ statData?.gRuleCount }}
+      </div>
+      <div>
+        {{ $t('system.casbin.stat.pRuleCount') }}: {{ statData?.pRuleCount }}
+      </div>
+      <div>
+        {{ $t('system.casbin.stat.subCount') }}: {{ statData?.subCount }}
+      </div>
+      <div>
+        {{ $t('system.casbin.stat.roleSubCount') }}:
+        {{ statData?.roleSubCount }}
+      </div>
+      <div>
+        {{ $t('system.casbin.stat.userSubCount') }}:
+        {{ statData?.userSubCount }}
+      </div>
+    </div>
   </Page>
 </template>
 <style scoped></style>
