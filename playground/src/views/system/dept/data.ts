9805f7839f6f1b6de6d26758d873bcd95d7517bc
eric <kuchaguangjie@gmail.com>
2026-01-20 16:28:58 +0800

    chore: ...

--- playground/src/views/system/dept/data.ts
+++ playground/src/views/system/dept/data.ts
@@ -1,12 +1,13 @@
 import type { VxeTableGridOptions } from '@vben/plugins/vxe-table';
 
 import type { VbenFormSchema } from '#/adapter/form';
-import { z } from '#/adapter/form';
 import type { OnActionClickFn } from '#/adapter/vxe-table';
 import type { SystemDeptApi } from '#/api/system/dept';
+
+import { z } from '#/adapter/form';
 import { $t } from '#/locales';
-import { formatBackendTime } from '#/utils/value-format';
 import { usePreviewLink } from '#/utils/use-preview-link';
+import { formatBackendTime } from '#/utils/value-format';
 
 /**
  * 获取编辑表单的字段配置。如果没有使用多语言，可以直接export一个数组常量
